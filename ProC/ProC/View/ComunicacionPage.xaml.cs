7bd2894180d61bd39c8076ddc2815cdc31df4446
McMario <McMario@DESKTOP-K90UHN2>
2019-05-07 22:56:09 -0400

    MarioG108
    Se agrego la funcionabilidad de enviar mail desde la pestaña comunicacion, utilizando el gestion de mail nativo del movil.

--- ProC/ProC/View/ComunicacionPage.xaml.cs
+++ ProC/ProC/View/ComunicacionPage.xaml.cs
@@ -6,15 +6,53 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace ProC.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ComunicacionPage : ContentPage
 	{
+        
 		public ComunicacionPage ()
 		{
-			InitializeComponent ();
-		}
-	}
+            InitializeComponent ();
+         }
+                            
+        private async void BtnSend_ClickedAsync(object sender, EventArgs e)
+        {
+            await SendMailAsync();
+        }
+
+        private async Task SendMailAsync() {
+            //comando para debugar System.Diagnostics.Debug.WriteLine("mensaje");
+            try
+            {       //lee el campo destinatarios como un string
+                var texto = txtDestino.Text;
+                //divide los destinatarios en una lista de destinatarios, deparadores dentro de {}
+               List<string> destinatarios = texto.Split(new Char[] { ' ', ',', ':', ';' }).ToList();
+                
+                var message = new EmailMessage
+                {
+                    Subject = txtAbout.Text,
+                    Body = txtBody.Text,
+                    To = destinatarios,
+
+                };
+                await Email.ComposeAsync(message);
+                { }
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+                System.Diagnostics.Debug.WriteLine(fbsEx);
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
+                System.Diagnostics.Debug.WriteLine(ex);
+
+            }
+        }
+    }
 }
